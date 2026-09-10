5e84ea6f50a3dc2200f1f7f0e8db866874586069
Alo <l24920217@voaxaca.tecnm.mx>
2026-09-10 01:51:26 -0600

    Eliminando funcion serie factorial

--- FactorialFor.js
+++ FactorialFor.js
@@ -1,27 +1,13 @@
 function factorial(n) {
   let resultado = 1;
-  
+
   for (let i = 1; i <= n; i++) {
     resultado *= i;
   }
-  
+
   return resultado;
 }
 
 // Ejemplo de uso
 console.log(factorial(5)); // 120
 
-function factorial(n) {
-    let f = 1;
-    let i = 1;
-    
-    while (i <= n) {
-        f = f * i;
-        i++;
-    }
-    
-    return f;
-}
-
-// Ejemplo de uso
-console.log(factorial(5)); // 120
