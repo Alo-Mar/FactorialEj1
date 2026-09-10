a51455ddd890b1430af91be32aa36d3a5a6fdaf6
Alo-Mar <l24920217@voaxaca.tecnm.mx>
2026-09-10 02:03:25 -0600

    Add factorial function and console log output

--- Factorial.js
+++ Factorial.js
@@ -9,3 +9,15 @@ function factorial(numero) {
 let numero = 5;
 
 console.log("El factorial de " + numero + " es: " + factorial(numero));
+
+function factorial(numero) {
+    if (numero === 0 || numero === 1) {
+        return 1;
+    }
+
+    return numero * factorial(numero - 1);
+}
+
+let numero = 5;
+
+console.log("El factorial de " + numero + " es: " + factorial(numero));
